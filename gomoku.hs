fbddc4fd74daac1d14d035124968ceda5225cf14
siorekoskar <siorek.oskar1@gmail.com>
2017-05-28 23:18:13 +0200

    Update gomoku.hs

--- gomoku.hs
+++ gomoku.hs
@@ -4,33 +4,37 @@ data Board = Board {size :: Int, cells :: [[Point]]}
 
 data Point = Point {x :: Int, y :: Int, color :: Color}
 
-data Color = Black | White | Empty
+data Color = Black | White | Empty deriving Eq
 
 instance Show Board where
     show (Board _ rows) = intercalate "\n" $ map show rows
 
 instance Show Point where
-    show (Point _ _ color) = show color
+    show (Point x y color) = show y
 
 instance Show Color where
     show Empty = " _ "
     show Black = " o "
     show White = " x "
 
--- makeBoard :: Int -> [[Point]]
--- makeBoard a = replicate a $ replicate a (Point 0 0 Empty)
-
 makeColumns :: Int -> Int -> [Point]-> [Point]
 makeColumns x y list
-    | y == 0 = list
+    | y == -1 = list
     | otherwise = makeColumns x (y-1) (b:list)
     where b = Point x y Empty
 
 makeBoard :: Int -> Int-> [[Point]] -> [[Point]]
 makeBoard x y list
-    | x == 0 = list
+    | x == -1 = list
     | otherwise = makeBoard (x-1) y (b:list)
     where b = makeColumns x y []
 
-board:: Board
-board = Board 19 (makeBoard 19 19 [])
+board :: Board
+board = Board 19 (makeBoard 18 18 [])
+
+insertFigure :: Board -> Int -> Int -> Color -> Board
+insertFigure board x y figure
+    | x > size board || y > size board || x < 0 || y < 0 = board
+    | color (cells board !! x !! y) == Empty = board
+    | color (cells board !! x !! y) == figure = board
+--    | otherwise = let (a,b) = splitAt 5 (cells board !! 2) in a ++ [Point 5 2 White] ++ (tail b)
