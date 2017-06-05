738810bc8d91e3cba52b9e382f67b0ac5d6deb8f
siorekoskar <siorek.oskar1@gmail.com>
2017-06-05 11:07:49 +0200

    Update gomoku.hs

--- gomoku.hs
+++ gomoku.hs
@@ -10,7 +10,7 @@ instance Show Board where
     show (Board _ rows) = intercalate "\n" $ map show rows
 
 instance Show Point where
-    show (Point x y color) = show y
+    show (Point x y color) = show color
 
 instance Show Color where
     show Empty = " _ "
@@ -20,6 +20,7 @@ instance Show Color where
 makeColumns :: Int -> Int -> [Point]-> [Point]
 makeColumns x y list
     | y == -1 = list
+--    | y == 9 && x == 9 = makeColumns x (y-1) ((Point x y Black):list) -- branch getposition
     | otherwise = makeColumns x (y-1) (b:list)
     where b = Point x y Empty
 
@@ -31,10 +32,20 @@ makeBoard x y list
 
 board :: Board
 board = Board 19 (makeBoard 18 18 [])
+newBoard = Board 19 (insertF board 5 8 Black)
 
 insertFigure :: Board -> Int -> Int -> Color -> Board
 insertFigure board x y figure
     | x > size board || y > size board || x < 0 || y < 0 = board
-    | color (cells board !! x !! y) == Empty = board
+    | color (cells board !! x !! y) == Empty = Board 19 (insertF board x y figure)
     | color (cells board !! x !! y) == figure = board
---    | otherwise = let (a,b) = splitAt 5 (cells board !! 2) in a ++ [Point 5 2 White] ++ (tail b)
+
+insertF :: Board -> Int -> Int -> Color -> [[Point]]
+insertF board x y figure =
+     let (a,b) = splitAt x (cells board)
+        in let d = [addPoint x y figure (head b)]
+            in a ++ (d) ++ (tail b)
+
+addPoint :: Int -> Int -> Color -> [Point] -> [Point]
+addPoint x y figure list =
+    let (a,b) = splitAt y list in (a ++ [Point x y figure] ++ (tail b))
